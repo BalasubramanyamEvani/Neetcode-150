528410678dbce591128e944f68c3c160d73d0830
E Bala <balasubramanyam.evani@gmail.com>
2023-06-20 03:01:18 -0400

    Time: 75 ms (46.62%), Space: 16.4 MB (87.89%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -0,0 +1,13 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        pos = [0, 0]
+        for move in moves:
+            if move == "U":
+                pos[1] += 1
+            elif move == "D":
+                pos[1] -= 1
+            elif move == "L":
+                pos[0] -= 1
+            else:
+                pos[0] += 1
+        return pos == [0, 0]
