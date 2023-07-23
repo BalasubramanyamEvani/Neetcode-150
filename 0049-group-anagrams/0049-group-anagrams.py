8bc38cf54f2040a4cca2521ba0ab5a759e9a3cec
E Bala <balasubramanyam.evani@gmail.com>
2023-07-22 17:15:47 -0700

    Time: 107 ms (89.85%), Space: 19.5 MB (95.68%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         mem = {}
-        keys = set()
         for s in strs:
             key = "".join(sorted(s))
-            if key in keys:
+            if key in mem:
                 mem[key].append(s)
             else:
                 mem[key] = [s]
-                keys.add(key)
         return [values for values in mem.values()]
