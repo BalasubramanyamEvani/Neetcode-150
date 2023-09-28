892fb0cb0ce6e9dddb1ed827985ed56c66431a57
E Bala <balasubramanyam.evani@gmail.com>
2023-09-28 11:37:30 -0400

    Time: 6830 ms (25.39%), Space: 16.3 MB (88.28%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,37 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        nr, nc = len(board), len(board[0])
+        
+        def dfs(res, r, c, index):
+            if len(res) == len(word):
+                return "".join(res) == word
+            
+            if r < 0 or r >= nr:
+                return False
+            
+            if c < 0 or c >= nc:
+                return False
+            
+            if word[index] != board[r][c]:
+                return False
+            
+            dr = [-1, 1, 0, 0]
+            dc = [0 ,0, -1, 1]
+            
+            res.append(board[r][c])
+            board[r][c] = "#"
+            
+            for i in range(4):
+                if dfs(res, r + dr[i], c + dc[i], index + 1):
+                    return True
+            
+            board[r][c] = res.pop(-1)
+            
+            return False
+        
+        for i in range(nr):
+            for j in range(nc):
+                if dfs([], i, j, 0):
+                    return True
+        
+        return False
