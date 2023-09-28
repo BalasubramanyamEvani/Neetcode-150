ae3a9a7c081b5774c1487175402938dbffd503ff
E Bala <balasubramanyam.evani@gmail.com>
2023-09-27 22:37:25 -0400

    Time: 34 ms (77.91%), Space: 16 MB (97.75%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,7 +3,5 @@ class Solution:
         dp = [0] * (n + 1)
         dp[0] = 1
         for i in range(1, n + 1):
-            one_step = dp[i - 1]
-            two_step = dp[i - 2]
-            dp[i] = one_step + two_step
+            dp[i] = dp[i - 1] + (dp[i - 2] if i - 2 >= 0 else 0)
         return dp[n]
