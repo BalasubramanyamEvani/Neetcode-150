36ff43fe5cc4c93af116f9fe40e2d3a1fe8ec70a
E Bala <balasubramanyam.evani@gmail.com>
2023-12-29 10:11:39 -0500

    Time: 592 ms (5.25%), Space: 20.3 MB (21.80%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        currsum = 0
+        N = len(nums)
+        ret = 0
+        mem = {}
+        for i in range(N):
+            currsum += nums[i]
+            if currsum == k:
+                ret += 1
+            if currsum - k in mem:
+                print(currsum, currsum - k)
+                ret += mem[currsum - k]
+            mem[currsum] = mem.get(currsum, 0) + 1
+        return ret
