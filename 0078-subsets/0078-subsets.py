307830c856ac67e706867fff35dbe28e6639fa93
E Bala <balasubramanyam.evani@gmail.com>
2024-01-07 11:42:32 -0500

    Time: 40 ms (62.93%), Space: 17.2 MB (28.85%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,12 +1,23 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        N = len(nums)
-        M = 2**N
+        # N = len(nums)
+        # M = 2**N
+        # ret = []
+        # for i in range(M):
+        #     tmp = []
+        #     for j in range(N):
+        #         if (i & (1 << j)) != 0:
+        #             tmp.append(nums[j])
+        #     ret.append(tmp)
+        # return ret
         ret = []
-        for i in range(M):
-            tmp = []
-            for j in range(N):
-                if (i & (1 << j)) != 0:
-                    tmp.append(nums[j])
-            ret.append(tmp)
+        N = len(nums)
+        def backtrack(tmp, ind):
+            ret.append(tmp[:])
+            for i in range(ind, N):
+                tmp.append(nums[i])
+                backtrack(tmp, i + 1)
+                tmp.pop()
+        
+        backtrack([], 0)
         return ret
