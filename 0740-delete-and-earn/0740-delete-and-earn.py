3c323288aa382029193efff86c88235fa097de6c
E Bala <balasubramanyam.evani@gmail.com>
2024-02-10 08:03:31 -0500

    Time: 79 ms (53.81%), Space: 18.6 MB (88.23%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -20,18 +20,25 @@ class Solution:
         # return recursion(maxnum, 0)
         
         # Top Down - Memoization
-        memo = {}
-        def recursion(num, tmp):
-            if num < 1:
-                return tmp
-            if num in memo:
-                return tmp + memo[num]
-            currcount = 0
-            if num in counts:
-                currcount = counts[num]
-            curr = currcount * num
-            l = recursion(num - 1, tmp)
-            r = recursion(num - 2, tmp + curr)
-            memo[num] = max(l, r)
-            return memo[num]
-        return recursion(maxnum, 0)
+        # memo = {}
+        # def recursion(num, tmp):
+        #     if num < 1:
+        #         return tmp
+        #     if num in memo:
+        #         return tmp + memo[num]
+        #     currcount = 0
+        #     if num in counts:
+        #         currcount = counts[num]
+        #     curr = currcount * num
+        #     l = recursion(num - 1, tmp)
+        #     r = recursion(num - 2, tmp + curr)
+        #     memo[num] = max(l, r)
+        #     return memo[num]
+        # return recursion(maxnum, 0)
+        
+        #Bottom Up - Tabular
+        dp = [0 for _ in range(maxnum + 1)]
+        dp[1] = counts.get(1, 0)
+        for i in range(2, maxnum + 1):
+            dp[i] = max(dp[i - 1], dp[i - 2] + i * counts.get(i, 0))
+        return dp[-1]
