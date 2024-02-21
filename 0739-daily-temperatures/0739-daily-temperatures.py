b4555ed3f356b217e2b4d93ade26dc9dd6f75680
E Bala <balasubramanyam.evani@gmail.com>
2024-02-21 07:22:02 -0500

    Time: 901 ms (53.93%), Space: 30.8 MB (69.13%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,18 +1,11 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        max_stack = collections.deque()
-        def add_to_stack(idx):
-            while max_stack and temperatures[max_stack[-1]] <=  temperatures[idx]:
-                max_stack.pop()
-            next_warmer_id = 0
-            if max_stack:
-                next_warmer_id = max_stack[-1] - idx
-            max_stack.append(idx)
-            return next_warmer_id
-        
-        ret = []
-        for i in range(len(temperatures) - 1, -1, -1):
-            next_warmer_id = add_to_stack(i)
-            ret.append(next_warmer_id)
-        return ret[::-1]
-        
+        stack = deque()
+        N = len(temperatures)
+        ret = [0 for _ in range(N)]
+        for i, temp in enumerate(temperatures):
+            while stack and stack[-1][1] < temp:
+                j, _ = stack.pop()
+                ret[j] = i - j
+            stack.append((i, temp))
+        return ret
