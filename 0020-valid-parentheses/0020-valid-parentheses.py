b40fea2c6ccd6731b5a107d2947f6f7183785244
E Bala <balasubramanyam.evani@gmail.com>
2024-02-22 19:28:20 -0500

    Time: 41 ms (27.41%), Space: 16.7 MB (46.83%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,41 +1,18 @@
-class Stack:
-    def __init__(self):
-        self.stack = []
-    
-    def push(self, ch):
-        self.stack.append(ch)
-    
-    def pop(self):
-        if not self.isEmpty():
-            ch = self.stack[-1]
-            self.stack.pop()
-            return ch
-    
-    def top(self):
-        if not self.isEmpty():
-            return self.stack[-1]
-    
-    def isEmpty(self):
-        return len(self.stack) == 0
-    
-    def __len__(self):
-        return len(self.stack)
-
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = Stack()
-        chmap = {
-            "}": "{",
+        stack = deque()
+        mem = {
             ")": "(",
+            "}": "{",
             "]": "["
         }
+        opened = mem.values()
         for ch in s:
-            if stack.isEmpty():
-                stack.push(ch)
-            elif ch == "(" or ch == "{" or ch == "[":
-                stack.push(ch)
-            elif chmap[ch] == stack.top():
+            if ch in opened:
+                stack.append(ch)
+            elif stack and mem[ch] == stack[-1]:
                 stack.pop()
             else:
                 return False
-        return stack.isEmpty()
+        return True if not stack else False
+        
