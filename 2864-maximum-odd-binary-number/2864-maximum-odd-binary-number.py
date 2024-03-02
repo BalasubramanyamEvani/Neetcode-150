e20ee54753a1942779a4e61251d687f2be882c10
E Bala <balasubramanyam.evani@gmail.com>
2024-03-02 18:57:40 -0500

    Time: 51 ms (5.58%), Space: 16.6 MB (17.78%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.py
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.py
@@ -1,11 +1,33 @@
 class Solution:
     def maximumOddBinaryNumber(self, s: str) -> str:
-        ones = 0
+        # Two pass solution
+        
+        # ones = 0
+        # N = len(s)
+        # for ch in s:
+        #     if ch == "1":
+        #         ones += 1
+        # lsb = 1 if ones >= 1 else 0
+        # zeros = N - ones
+        # ones = ones - lsb
+        # return "1" * ones + "0" * zeros + "1" * lsb
+        
+        # One pass solution
         N = len(s)
-        for ch in s:
-            if ch == "1":
-                ones += 1
-        lsb = 1 if ones >= 1 else 0
-        zeros = N - ones
-        ones = ones - lsb
-        return "1" * ones + "0" * zeros + "1" * lsb
+        l = 0
+        r = N - 1
+        arr = [ch for ch in s]
+        while l <= r:
+            if arr[l] == "1":
+                l += 1
+            if arr[r] == "0":
+                r -= 1
+            if l <= r and arr[l] < arr[r]:
+                arr[l], arr[r] = arr[r], arr[l]
+                l += 1
+                r -= 1
+        last_one = l - 1
+        if last_one >= 0 and last_one != N - 1:
+            arr[last_one] = "0"
+            arr[-1] = "1"
+        return "".join(arr)
