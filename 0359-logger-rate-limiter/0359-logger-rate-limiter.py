8ba1f3d97718c06aa57690dcb7aa2243c810c617
E Bala <balasubramanyam.evani@gmail.com>
2024-03-08 08:29:23 -0500

    Time: 131 ms (9.86%), Space: 23.2 MB (41.46%) - LeetHub

--- 0359-logger-rate-limiter/0359-logger-rate-limiter.py
+++ 0359-logger-rate-limiter/0359-logger-rate-limiter.py
@@ -1,11 +1,26 @@
 class Logger:
     def __init__(self, limiter=10):
         self.limiter = limiter
-        self.messages = {}
+        # self.messages = {}
+        self.messages = deque()
+        self.uniq_elements = set()
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        if message not in self.messages or self.messages[message] <= timestamp:
-            self.messages[message] = timestamp + self.limiter
+        # Hashmap Solution
+        
+        # if message not in self.messages or self.messages[message] <= timestamp:
+        #     self.messages[message] = timestamp + self.limiter
+        #     return True
+        # return False
+        
+        # Queue solution - better since old messages can also be deleted while checking
+        while self.messages and self.messages[0][1] <= timestamp:
+            msgItem = self.messages.popleft()
+            self.uniq_elements.remove(msgItem[0])
+        if message not in self.uniq_elements:
+            # works because chronological order
+            self.messages.append((message, timestamp + self.limiter))
+            self.uniq_elements.add(message)
             return True
         return False
 
