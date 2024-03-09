afa89bc31efedb8ad6b45deb45c98487fce1d9ff
E Bala <balasubramanyam.evani@gmail.com>
2024-03-08 22:56:13 -0500

    Time: 124 ms (72.65%), Space: 18.2 MB (44.94%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,16 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        def swap(a, b):
-            nums[a], nums[b] = nums[b], nums[a]
-
-        lastzero_index = -1
-        i = 0
-        numslen = len(nums)
-        while i < numslen:
-            if nums[i] == 0 and lastzero_index == -1:
-                lastzero_index = i
-            if nums[i] != 0 and lastzero_index >= 0:
-                swap(i, lastzero_index)
-                lastzero_index += 1
-            i += 1
+        N = len(nums)
+        r = 0
+        zeroindex = -1
+        while r < N:
+            if nums[r] == 0 and zeroindex == -1:
+                zeroindex = r
+            elif nums[r] != 0 and zeroindex != -1:
+                nums[zeroindex], nums[r] = nums[r], nums[zeroindex]
+                zeroindex += 1
+            r += 1
