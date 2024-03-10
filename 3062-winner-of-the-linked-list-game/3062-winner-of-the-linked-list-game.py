75ec07d9bf313ca6886f99268ace516fdb2a8334
E Bala <balasubramanyam.evani@gmail.com>
2024-03-10 03:06:29 -0400

    Time: 44 ms (34.07%), Space: 16.7 MB (13.96%) - LeetHub

---
+++ 3062-winner-of-the-linked-list-game/3062-winner-of-the-linked-list-game.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def gameResult(self, head: Optional[ListNode]) -> str:
+        odd = 0
+        even = 0
+        curr = head
+        while curr:
+            evennum = curr.val
+            oddnum = curr.next.val
+            if oddnum > evennum:
+                odd += 1
+            elif evennum > oddnum:
+                even += 1
+            curr = curr.next.next
+        if odd == even:
+            return "Tie"
+        return "Odd" if odd > even else "Even"
+        
