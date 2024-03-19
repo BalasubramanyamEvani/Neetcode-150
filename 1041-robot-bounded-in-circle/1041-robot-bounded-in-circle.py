128da8d4f6775ca86b8eb896373485b2f1dde5da
E Bala <balasubramanyam.evani@gmail.com>
2024-03-19 11:22:04 -0400

    Time: 37 ms (46.21%), Space: 16.7 MB (7.63%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
@@ -3,13 +3,14 @@ class Solution:
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         x, y = 0, 0
         rot = 0
-        for instruction in instructions:
-            if instruction == "R":
-                rot = (rot + 1) % 4
-            elif instruction == "L":
-                rot = (rot - 1) % 4
-            else:
-                x += directions[rot][0]
-                y += directions[rot][1]
+        for i in range(4):
+            for instruction in instructions:
+                if instruction == "R":
+                    rot = (rot + 1) % 4
+                elif instruction == "L":
+                    rot = (rot - 1) % 4
+                else:
+                    x += directions[rot][0]
+                    y += directions[rot][1]
         
-        return x == 0 and y == 0 or rot != 0
+        return x == 0 and y == 0
