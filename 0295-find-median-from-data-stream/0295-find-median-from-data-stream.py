77ba3ba9fe526491fe0a1552d06a136fd80ba4ed
E Bala <balasubramanyam.evani@gmail.com>
2024-03-26 17:02:57 -0400

    Time: 382 ms (49.72%), Space: 39.2 MB (5.76%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -4,31 +4,39 @@ class MedianFinder:
         self.high_min_heap = []
 
     def addNum(self, num: int) -> None:
+        # insert to low min heap
         heapq.heappush(self.low_max_heap, -num)
-        if self.high_min_heap:
-            high_min = heapq.heappop(self.high_min_heap)
-            heapq.heappush(self.low_max_heap, -high_min)
         
-        while len(self.low_max_heap) - len(self.high_min_heap) > 1:
+        # if the maximum in min heap is not <= minimum in high max heap
+        # then pop from low and push to high
+        if self.high_min_heap and -self.low_max_heap[0] > self.high_min_heap[0]:
             low_max = heapq.heappop(self.low_max_heap)
             heapq.heappush(self.high_min_heap, -low_max)
-        
+        # if size difference > 1 then we need to rebalance it
+        lN, hN = len(self.low_max_heap), len(self.high_min_heap)
+        if lN - hN > 1:
+            low_max = heapq.heappop(self.low_max_heap)
+            heapq.heappush(self.high_min_heap, -low_max)
+        elif hN - lN > 1:
+            high_min = heapq.heappop(self.high_min_heap)
+            heapq.heappush(self.low_max_heap, -high_min)
+
     def findMedian(self) -> float:
-        assert len(self.low_max_heap) - len(self.high_min_heap) <= 1
-        
-        low_max = -self.low_max_heap[0]
-        
+        lN, hN = len(self.low_max_heap), len(self.high_min_heap)
         # only one element
-        if not self.high_min_heap:
-            return low_max
+        if hN == 0:
+            return -self.low_max_heap[0]
         
         # odd elements
-        if len(self.low_max_heap) - len(self.high_min_heap) == 1:
-            return low_max
-        
-        high_min = self.high_min_heap[0]
-        
+        if hN != lN:
+            if lN > hN:
+                return -self.low_max_heap[0]
+            else:
+                return self.high_min_heap[0]
+
         # even elements
+        low_max = -self.low_max_heap[0]
+        high_min = self.high_min_heap[0]
         return (low_max + high_min) / 2
 
 
