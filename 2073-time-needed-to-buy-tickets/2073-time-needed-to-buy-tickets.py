86739c7d4bd854e9158b001d500bb3bb638927b5
E Bala <balasubramanyam.evani@gmail.com>
2024-04-09 16:35:50 -0400

    Time: 30 ms (98.03%), Space: 16.6 MB (51.50%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        res = 0
+        for i, ticket in enumerate(tickets):
+            # the number of times the front numbers
+            # would come infront of the ticket at kth
+            # position
+            if i <= k :
+                res += min(ticket, tickets[k])
+            # the number of times the back numbers
+            # would come behind the ticket at kth
+            # position
+            else:
+                res += min(tickets[k] - 1, ticket)
+        return res
