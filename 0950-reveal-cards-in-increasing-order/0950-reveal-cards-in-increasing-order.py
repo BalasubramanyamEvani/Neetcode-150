a87603ab1470cd475644c47c3780e204a6558100
E Bala <balasubramanyam.evani@gmail.com>
2024-04-10 19:55:38 -0400

    Time: 52 ms (18.20%), Space: 16.8 MB (36.91%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
@@ -2,10 +2,18 @@ class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
         N = len(deck)
         res = [0] * N
+        # the sorted order
         deck = sorted(deck)
+        # simulation using indices
         q = deque([i for i in range(N)])
         ptr = 0
         while q:
+            # insert at respective positions
+            # [0, 1, 2, 3, 4, 5, 6]
+            # ✔︎ x ✔︎ x ✔︎ x ✔︎
+            #   ✔︎   x   ✔︎
+            #.      ✔︎   x
+            #           ✔︎
             index = q.popleft()
             res[index] = deck[ptr]
             if q:
