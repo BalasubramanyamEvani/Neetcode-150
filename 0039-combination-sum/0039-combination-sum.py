b49fbef151cc3a34e3cd606e6232e8d71607f1a7
E Bala <balasubramanyam.evani@gmail.com>
2024-04-11 17:20:20 -0400

    Time: 49 ms (75.07%), Space: 16.8 MB (15.81%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -2,6 +2,12 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:            
         ret = []
         N = len(candidates)
+        # important things i noticed
+        # 1. distinct numbers
+        # 2. combination 2 2 3 can be written as
+        # 2 3 2 or 3 3 2, but all are same, so its better
+        # to just output the sorted order (indices) which
+        # will be traversed while doing DFS
         def dfs(tmp, index, currsum):
             if currsum == target:
                 ret.append(tmp[:])
