efcef146d30f35b6df95f046af02a9094b6b3779
E Bala <balasubramanyam.evani@gmail.com>
2024-04-11 18:01:29 -0400

    Time: 42 ms (39.39%), Space: 16.8 MB (50.48%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,11 +1,16 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         ret = []
+        # sort in increasing order
+        # to make sure we don't visit
+        # numbers already visited after
+        # certain index
         nums.sort()
         N = len(nums)
         def dfs(index, tmp):
             ret.append(tmp[:])
             for i in range(index + 1, N):
+                # this is where the check happens
                 if nums[i] == nums[i - 1] and i - 1 != index:
                     continue
                 tmp.append(nums[i])
