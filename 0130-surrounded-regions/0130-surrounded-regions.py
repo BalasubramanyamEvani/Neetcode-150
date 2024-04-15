0a30b1be6423741adfdd7cc29f455f6fea733197
E Bala <balasubramanyam.evani@gmail.com>
2024-04-15 11:20:27 -0400

    Time: 115 ms (82.64%), Space: 17.9 MB (55.81%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,33 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        nr, nc = len(board), len(board[0])
+        visited = set()
+        def dfs(r, c):
+            if r < 0 or r >= nr or c < 0 or c >= nc:
+                return
+            if board[r][c] == "O" and (r, c) not in visited:
+                visited.add((r, c))
+                dfs(r + 1, c)
+                dfs(r - 1, c)
+                dfs(r, c + 1)
+                dfs(r, c - 1)
+        
+        for i in range(nr):
+            if board[i][0] == "O" and (i, 0) not in visited: 
+                dfs(i, 0)
+            if board[i][nc - 1] == "O" and (i, nc - 1) not in visited:
+                dfs(i, nc - 1)
+        
+        for i in range(nc):
+            if board[0][i] == "O" and (0, i) not in visited: 
+                dfs(0, i)
+            if board[nr - 1][i] == "O" and (nr - 1, i) not in visited:
+                dfs(nr - 1, i)
+        
+        for i in range(nr):
+            for j in range(nc):
+                if (i, j) not in visited:
+                    board[i][j] = "X"
