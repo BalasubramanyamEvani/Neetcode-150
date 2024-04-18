c6ba6c9bf342dbdad475e5edbcd5c727c8242240
E Bala <balasubramanyam.evani@gmail.com>
2024-04-17 22:10:24 -0400

    Time: 460 ms (29.00%), Space: 26.7 MB (5.30%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -2,27 +2,28 @@ class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         nr, nc = len(grid), len(grid[0])
         visited = set()
-        count = 0
-        def valid(r, c):
-            return r >= 0 and r < nr and c >= 0 and c < nc
-            
-        def recursive(r, c):
-            if not valid(r, c):
+        # 1 represents there is a blockage
+        # 0 represents there is no blockage
+        def dfs(r, c):
+            if r < 0 or r >= nr or c < 0 or c >= nc:
                 return 0
             if (r, c) not in visited and grid[r][c] == 1:
                 visited.add((r, c))
-                nonlocal count
-                down = recursive(r + 1, c)
-                up = recursive(r - 1, c)
-                right = recursive(r, c + 1)
-                left = recursive(r, c - 1)
-                count += 4 - (up + down + right + left)
+                nonlocal ret
+                bottom = dfs(r + 1, c)
+                top = dfs(r - 1, c)
+                right = dfs(r, c + 1)
+                left = dfs(r, c - 1)
+                ret += 4 - bottom - top - right - left
                 return 1
-            return 1 if grid[r][c] == 1 else 0
+            if grid[r][c] == 0:
+                return 0
+            return 1
         
+        ret = 0
         for r in range(nr):
             for c in range(nc):
                 if grid[r][c] == 1:
-                    recursive(r, c)
+                    dfs(r, c)
                     break
-        return count
+        return ret
