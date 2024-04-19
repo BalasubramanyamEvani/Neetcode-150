c1b3e3ef4626dcd1c640641fc5c2f7b2a4768d00
E Bala <balasubramanyam.evani@gmail.com>
2024-04-19 00:39:00 -0400

    Time: 44 ms (5.75%), Space: 16.5 MB (89.63%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -2,18 +2,29 @@ class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         if not nums:
             return []
+        # below will not work for duplicate numbers
         curr = []
         ret = []
         for i, num in enumerate(nums):
             if not curr:
                 curr.append(num)
             if i - 1 >= 0 and num != nums[i - 1] + 1:
+                # when consecutive numbers are not observed
+                # add this range
+                # before adding end point
+                # ensure its not same as start point
                 if nums[i - 1] != curr[0]:
                     curr.append(nums[i - 1])
                 ret.append(curr)
                 curr = [num]
+        # to handle range end in the end
+        # if any curr is left to add
         if curr:
             ret.append(curr)
+        # above statement ensures the start point
+        # of range was added below ensures the
+        # last point was also added because last range
+        # should end with last num in the array
         if nums[-1] != ret[-1][-1]:
             curr.append(nums[-1])
         ret = [f"{l[0]}->{l[1]}" if len(l) == 2 else f"{l[0]}" for l in ret]
