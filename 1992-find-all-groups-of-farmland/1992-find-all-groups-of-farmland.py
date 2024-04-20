9a7a45c335552d6390cd0b2e1d8ca81cb8b9082d
E Bala <balasubramanyam.evani@gmail.com>
2024-04-20 00:29:59 -0400

    Time: 1057 ms (17.00%), Space: 47.8 MB (12.00%) - LeetHub

---
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
+        nr, nc = len(land), len(land[0])
+        groups = []
+        
+        visited = set()
+        def dfs(r, c, group):
+            if r < 0 or r >= nr or c < 0 or c >= nc:
+                return
+            if (r, c) not in visited and land[r][c] == 1:
+                visited.add((r, c))
+                group[0], group[1] = min(group[0], r), min(group[1], c)
+                group[2], group[3] = max(group[2], r), max(group[3], c)
+                dfs(r + 1, c, group)
+                dfs(r - 1, c, group)
+                dfs(r, c + 1, group)
+                dfs(r, c - 1, group)
+        
+        for r in range(nr):
+            for c in range(nc):
+                if (r, c) not in visited and land[r][c] == 1:
+                    group = [r, c, r, c]
+                    dfs(r, c, group)
+                    groups.append(group)
+        
+        return groups
