a6417a031c51ac4b9bdcf1afc438a45cec7ca070
E Bala <balasubramanyam.evani@gmail.com>
2024-04-25 13:14:55 -0400

    Time: 30 ms (87.20%), Space: 16.8 MB (6.01%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,8 +1,16 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n == 1:
-            return x
-        tmp = pow(x, n // 2)
-        if n % 2 == 0:
-            return tmp * tmp
-        return x * tmp * tmp
+        def dfs(a, N):
+            if N == 0:
+                return 1
+            if N == 1:
+                return a
+            if N < 0:
+                num = 1.0 / dfs(a, -N // 2)
+            else:
+                num = dfs(a, N // 2)
+            if N % 2 == 0:
+                return num * num
+            return num * num * (a if N > 0 else 1 / a)
+        # log(N) complexity
+        return dfs(x, n)
