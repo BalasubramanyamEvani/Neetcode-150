9af2ae7a333e358e3c985fd8cbe3021e8cc436e4
E Bala <balasubramanyam.evani@gmail.com>
2024-04-28 21:41:50 -0400

    Time: 649 ms (11.48%), Space: 77.8 MB (46.04%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,95 +1,133 @@
-class Node:
-    def __init__(self, key, val):
+class CacheItem:
+    def __init__(
+        self,
+        key: int,
+        val: int,
+        prevPtr: "CacheItem" = None,
+        nextPtr: "CacheItem" = None,
+    ) -> None:
         self.key = key
         self.val = val
-        self.next = None
-        self.prev = None
-    
-    def __repr__(self):
-        return f"Node(key={self.key}, value={self.val})"
+        self.nextPtr = nextPtr
+        self.prevPtr = prevPtr
+
+    def __str__(self) -> str:
+        return f"[CacheItem(prev={self.prevPtr}, key={self.key}, val={self.val}, next={self.nextPtr.key}]"
 
 
-class Queue:
-    def __init__(self):
+class CacheQueue:
+    def __init__(self) -> None:
         self.head = None
         self.tail = None
-    
-    def insert_at_head(self, key, val):
-        newnode = Node(key, val)
-        if not self.head:
-            self.head = newnode
-            self.tail = newnode
-        else:
-            newnode.next = self.head
-            self.head.prev = newnode
-            self.head = newnode
-        return newnode
-    
-    def move_to_head(self, node):
-        if self.head == node or not node:
-            return self.head
-        prevnode = node.prev
-        nextnode = node.next
-        if prevnode:
-            prevnode.next = nextnode
-        else:
-            self.head = nextnode
-        node.prev = None
-        if nextnode:
-            nextnode.prev = prevnode
-        else:
-            self.tail = prevnode
-        node.next = None
-        return self.insert_at_head(node.key, node.val)
+        self.capacity = 0
 
-    def pop_from_tail(self):
+    def appendleft(self, key: int, val: int) -> CacheItem:
+        item = CacheItem(key, val)
+        # increment size
+        self.capacity += 1
+        # if head is not present
         if not self.head:
-            return -1
-        tailnode = self.tail
-        prevnode = self.tail.prev
-        if prevnode:
-            prevnode.next = None
-        self.tail.prev = None
-        self.tail = prevnode
-        if not self.tail:
-            self.head = None
-        return tailnode.key
-    
-    def __str__(self):
-        curr = self.head
-        ret = []
-        while curr:
-            ret.append(curr.__repr__())
-            curr = curr.next
-        return " ".join(ret)
+            self.head = item
+            self.tail = item
+            return item
+        # if head is present
+        item.nextPtr = self.head
+        self.head.prevPtr = item
+        self.head = item
+        return item
+
+    def remove(self, item: CacheItem) -> tuple[int, int]:
+        # trying to remove when queue is empty
+        if self.capacity == 0:
+            raise RuntimeError(f"Invalid operation remove for item: {item}")
+
+        nextItem = item.nextPtr
+        prevItem = item.prevPtr
+        # if prev item is present connect to item.next
+        if prevItem:
+            prevItem.nextPtr = nextItem
+        # else its head so update head
+        else:
+            self.head = nextItem
+        # if next item is present
+        if nextItem:
+            nextItem.prevPtr = prevItem
+        else:
+            self.tail = prevItem
+        # make sure head prev is None
+        if self.head:
+            self.head.prevPtr = None
+        # make sure tail next is None
+        if self.tail:
+            self.tail.nextPtr = None
+        # cleanup
+        item.nextPtr = None
+        item.prevPtr = None
+        # decrement size
+        self.capacity -= 1
+        return item.key, item.val
+
+    def pop(self) -> CacheItem:
+        # if queue is empty return none
+        if self.capacity == 0:
+            return None
+
+        prevTail = self.tail
+        self.tail = prevTail.prevPtr
+        if self.tail:
+            self.tail.nextPtr = None
+        else:
+            # only item removed
+            self.head = self.tail = None
+        # cleanup
+        prevTail.prevPtr = None
+        # decrement capacity
+        self.capacity -= 1
+        return prevTail
+
+    def movetohead(self, item: CacheItem) -> CacheItem:
+        # removing item
+        key, val = self.remove(item)
+        # moving to head (head has higher priority)
+        return self.appendleft(key, val)
+
+    def size(self) -> int:
+        return self.capacity
+
 
 class LRUCache:
-    def __init__(self, capacity: int):
+    def __init__(self, capacity: int) -> None:
         self.capacity = capacity
-        self.q = Queue()
-        self.mem = {}
+        self.cacheq = CacheQueue()
+        self.mem: dict[int, CacheItem] = {}
 
     def get(self, key: int) -> int:
         if key in self.mem:
-            node = self.mem[key]
-            newnode = self.q.move_to_head(node)
-            self.mem[key] = newnode
-            return newnode.val
+            item = self.mem[key]
+            # recently accessed
+            self.mem[key] = self.cacheq.movetohead(item)
+            return self.mem[key].val
+        # else item is not present
         return -1
 
-    def put(self, key: int, value: int) -> None:
-        if key in self.mem:
-            node = self.mem[key]
-            node.val = value
-            newnode = self.q.move_to_head(node)
-            self.mem[key] = newnode
+    def put(self, key: int, val: int) -> None:
+        # if new item
+        if key not in self.mem:
+            # if cacheq size limit reached then pop from tail
+            # to create space
+            if self.cacheq.size() == self.capacity:
+                poppedItem = self.cacheq.pop()
+                # delete from map as well
+                del self.mem[poppedItem.key]
+            # insert item at head
+            item = self.cacheq.appendleft(key, val)
+            self.mem[item.key] = item
         else:
-            if self.capacity == 0:
-                oldkey = self.q.pop_from_tail()
-                del self.mem[oldkey]
-                self.capacity += 1
-            self.mem[key] = self.q.insert_at_head(key, value)
-            self.capacity -= 1
+            item = self.mem[key]
+            # update value
+            item.val = val
+            # recently accessed
+            self.mem[key] = self.cacheq.movetohead(item)
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
