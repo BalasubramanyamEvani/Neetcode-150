7951905940835f9655d57d12360e6a89e9bff493
E Bala <balasubramanyam.evani@gmail.com>
2024-05-19 20:20:36 -0400

    Time: 390 ms (25.12%), Space: 17.7 MB (87.69%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,13 +1,21 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         N = len(nums)
-        @cache
-        def dfs(i):
-            if i == N - 1:
-                return True
-            jumps = min(N - 1, i + nums[i])
-            for j in range(i + 1, jumps + 1):
-                if dfs(j):
-                    return True
-            return False
-        return dfs(0)
+        # @cache
+        # def dfs(i):
+        #     if i == N - 1:
+        #         return True
+        #     jumps = min(N - 1, i + nums[i])
+        #     for j in range(i + 1, jumps + 1):
+        #         if dfs(j):
+        #             return True
+        #     return False
+        # return dfs(0)
+        
+        # greedy
+        currmax = 0
+        for i, num in enumerate(nums):
+            if i > currmax:
+                break
+            currmax = max(currmax, i + nums[i])
+        return currmax >= N - 1
