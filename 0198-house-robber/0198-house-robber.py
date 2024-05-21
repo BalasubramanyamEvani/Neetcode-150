4a9ca2bc22c18935da3fd38b52c08b9a03ced25b
E Bala <balasubramanyam.evani@gmail.com>
2024-05-21 00:45:10 -0400

    Time: 39 ms (33.68%), Space: 16.7 MB (12.40%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -5,5 +5,4 @@ class Solution:
         dp[0] = nums[0]
         for i in range(1, len(nums)):
             dp[i] = max(dp[i - 1], nums[i] + (dp[i - 2] if i - 2 >= 0 else 0))
-        print(dp)
         return dp[-1]
