985584313b6c2204c1a8c42b10979bdac198c787
E Bala <balasubramanyam.evani@gmail.com>
2024-05-21 00:45:06 -0400

    Time: 39 ms (33.68%), Space: 16.5 MB (44.76%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        tot_houses = len(nums)
+        dp = [0] * tot_houses
+        dp[0] = nums[0]
+        for i in range(1, len(nums)):
+            dp[i] = max(dp[i - 1], nums[i] + (dp[i - 2] if i - 2 >= 0 else 0))
+        print(dp)
+        return dp[-1]
