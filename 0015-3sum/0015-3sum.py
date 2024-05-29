45a748c17977a9ae8d1f14dc52f9ea575e45e9de
E Bala <balasubramanyam.evani@gmail.com>
2024-05-29 08:48:40 -0400

    Time: 600 ms (82.42%), Space: 20.8 MB (26.67%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,26 +1,22 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        def twoSum(i, val):
-            left = i + 1
-            right = len(nums) - 1
+        N = len(nums)
+        def twoSum(left, n):
+            right = N - 1
             while left < right:
-                if nums[left] + nums[right] == val:
-                    triplets.append([nums[i], nums[left], nums[right]])
-                    left += 1
-                    while nums[left] == nums[left - 1] and left < right:
-                        left += 1
-                elif nums[left] + nums[right] < val:
-                    left += 1
-                elif nums[left] + nums[right] > val:
+                if nums[left] + nums[right] > n:
                     right -= 1
-
-        triplets = []
+                elif nums[left] + nums[right] < n:
+                    left += 1
+                else:
+                    ret.append([-n, nums[left], nums[right]])
+                    left += 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+        ret = []
         nums = sorted(nums)
-        for i in range(len(nums)):
+        for i, num in enumerate(nums):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            num1 = nums[i]
-            pairs = twoSum(i, -num1)
-        
-        return triplets
-        
+            twoSum(i + 1, -num)
+        return ret
