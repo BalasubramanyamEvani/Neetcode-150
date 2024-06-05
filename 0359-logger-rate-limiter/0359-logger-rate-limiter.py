bc3b926d08db296e94396a93eb790c7a58bacfaf
E Bala <balasubramanyam.evani@gmail.com>
2024-06-05 09:40:13 -0400

    Time: 107 ms (96.89%), Space: 23.1 MB (70.31%) - LeetHub

--- 0359-logger-rate-limiter/0359-logger-rate-limiter.py
+++ 0359-logger-rate-limiter/0359-logger-rate-limiter.py
@@ -1,6 +1,6 @@
 class Logger:
-    def __init__(self, limiter=10):
-        self.limiter = limiter
+    def __init__(self, t=10):
+        self.t = t
         # self.messages = {}
         self.messages = deque()
         self.uniq_elements = set()
@@ -14,12 +14,14 @@ class Logger:
         # return False
         
         # Queue solution - better since old messages can also be deleted while checking
+        # while loop to discard old elements
         while self.messages and self.messages[0][1] <= timestamp:
             msgItem = self.messages.popleft()
             self.uniq_elements.remove(msgItem[0])
+        # now new message not in queue can print it
         if message not in self.uniq_elements:
             # works because chronological order
-            self.messages.append((message, timestamp + self.limiter))
+            self.messages.append((message, timestamp + self.t))
             self.uniq_elements.add(message)
             return True
         return False
