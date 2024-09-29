e63b8630db9663f191c81bf3d40d21f6d43772a5
E Bala <balasubramanyam.evani@gmail.com>
2024-09-28 20:55:39 -0400

    Time: 32 ms (89.47%), Space: 17.5 MB (9.73%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -9,25 +9,44 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        if not head:
-            return
-        # interleave
-        curr = head
-        while curr:
-            tmp = curr.next
-            curr.next = Node(curr.val)
-            curr.next.next = tmp
-            curr = tmp
-        # copy random pointers
-        curr = head
-        while curr:
-            curr.next.random = curr.random.next if curr.random else None
-            curr = curr.next.next
-        original, copy = head, head.next
-        ret = head.next
-        while original:
-            original.next = original.next.next
-            copy.next = copy.next.next if copy.next else None
-            copy = copy.next
-            original = original.next
-        return ret
+        # if not head:
+        #     return
+        # # interleave
+        # curr = head
+        # while curr:
+        #     tmp = curr.next
+        #     curr.next = Node(curr.val)
+        #     curr.next.next = tmp
+        #     curr = tmp
+        # # copy random pointers
+        # curr = head
+        # while curr:
+        #     curr.next.random = curr.random.next if curr.random else None
+        #     curr = curr.next.next
+        # original, copy = head, head.next
+        # ret = head.next
+        # while original:
+        #     original.next = original.next.next
+        #     copy.next = copy.next.next if copy.next else None
+        #     copy = copy.next
+        #     original = original.next
+        # return ret
+        mem = {}
+        curr1 = head
+        copy = Node(-1)
+        curr2 = copy
+        while curr1:
+            curr2.next = Node(curr1.val)
+            mem[curr1] = curr2.next
+            curr1 = curr1.next
+            curr2 = curr2.next
+        
+        curr1 = head
+        curr2 = copy.next
+        while curr1:
+            curr2.random = mem[curr1.random] if curr1.random else None
+            curr1 = curr1.next
+            curr2 = curr2.next
+        return copy.next
+
+            
