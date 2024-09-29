9823c18915bac9a47e15c2afb79339cd35c99028
E Bala <balasubramanyam.evani@gmail.com>
2024-09-28 20:00:16 -0400

    Time: 40 ms (43.54%), Space: 17.6 MB (9.73%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -9,36 +9,25 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        mem1, mem2, mem3 = {}, {}, {}
-        curr = head
-        i = 0
-        while curr:
-            mem1[curr] = i
-            i += 1
-            curr = curr.next
+        if not head:
+            return
+        # interleave
         curr = head
         while curr:
-            mem2[mem1[curr]] = mem1[curr.random] if curr.random else -1
-            curr = curr.next
-        dummy = Node(-1)
-        curr1 = dummy
-        curr2 = head
-        while curr2:
-            curr1.next = Node(curr2.val, None, None)
-            curr1 = curr1.next
-            curr2 = curr2.next
-        curr = dummy.next
-        if not curr:
-            return None
-        i = 0
+            tmp = curr.next
+            curr.next = Node(curr.val)
+            curr.next.next = tmp
+            curr = tmp
+        # copy random pointers
+        curr = head
         while curr:
-            mem3[i] = curr
-            curr = curr.next
-            i += 1
-        curr = dummy.next
-        i = 0
-        while curr:
-            curr.random = mem3[mem2[i]] if mem2[i] >= 0 else None
-            curr = curr.next
-            i += 1
-        return dummy.next
+            curr.next.random = curr.random.next if curr.random else None
+            curr = curr.next.next
+        original, copy = head, head.next
+        ret = head.next
+        while original:
+            original.next = original.next.next
+            copy.next = copy.next.next if copy.next else None
+            copy = copy.next
+            original = original.next
+        return ret
