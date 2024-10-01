ca289664832ae5b82be4bc67d80a4cdf89e861d7
E Bala <balasubramanyam.evani@gmail.com>
2024-10-01 00:02:02 -0400

    Time: 47 ms (30.45%), Space: 17.9 MB (11.88%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -13,12 +13,10 @@ class Solution:
         if not root:
             return
         q = deque([(0, root)])
-        cache = {}
+        cache = defaultdict(list)
         while q:
             level, node = q.popleft()
-            if level not in cache:
-                cache[level] = []
-            elif cache[level]:
+            if cache[level]:
                 cache[level][-1].next = node
             cache[level].append(node)
             for child in [node.left, node.right]:
