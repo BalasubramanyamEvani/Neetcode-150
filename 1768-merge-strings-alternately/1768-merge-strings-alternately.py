a0d5fd270b7100fba5c58e8d237dde4a5377d7fd
E Bala <balasubramanyam.evani@gmail.com>
2024-10-30 00:15:18 -0400

    Time: 31 ms (79.85%), Space: 16.6 MB (10.84%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,23 +1,20 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        joined_str = []
-        word1_start, word1_end = 0, len(word1)  - 1
-        word2_start, word2_end = 0, len(word2) - 1
-        while word1_start <= word1_end and word2_start <= word2_end:
-            joined_str.append(word1[word1_start])
-            joined_str.append(word2[word2_start])
-            
-            word1_start += 1
-            word2_start += 1
-        
-        while word1_start <= word1_end:
-            joined_str.append(word1[word1_start])
-            word1_start += 1
-        
-        while word2_start <= word2_end:
-            joined_str.append(word2[word2_start])
-            word2_start += 1
-        
-        joined_str = "".join(joined_str)
-        return joined_str
-            
+        ptr1, ptr2 = 0, 0
+        ret = []
+        switch = False
+        while ptr1 < len(word1) and ptr2 < len(word2):
+            if not switch:
+                ret.append(word1[ptr1])
+                ptr1 += 1
+            else:
+                ret.append(word2[ptr2])
+                ptr2 += 1
+            switch = ~switch
+        while ptr1 < len(word1):
+            ret.append(word1[ptr1])
+            ptr1 += 1
+        while ptr2 < len(word2):
+            ret.append(word2[ptr2])
+            ptr2 += 1
+        return "".join(ret)
