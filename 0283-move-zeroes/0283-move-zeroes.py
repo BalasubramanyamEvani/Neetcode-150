3b8e3c620fb64c4a998cc838475c8217851011c9
E Bala <balasubramanyam.evani@gmail.com>
2024-11-10 02:49:16 -0500

    Time: 2 ms (85.81%), Space: 17.7 MB (94.95%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,13 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        N = len(nums)
-        r = 0
-        zeroindex = -1
-        while r < N:
-            if nums[r] == 0 and zeroindex == -1:
-                zeroindex = r
-            elif nums[r] != 0 and zeroindex != -1:
-                nums[zeroindex], nums[r] = nums[r], nums[zeroindex]
-                zeroindex += 1
-            r += 1
+        zero = -1
+        for i, num in enumerate(nums):
+            if num == 0 and zero == -1:
+                zero = i
+            elif num != 0 and zero >= 0:
+                nums[zero], nums[i] = nums[i], nums[zero]
+                zero += 1
+            
