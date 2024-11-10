c5b7abb01c8423cefdf783d464bf073d2e974eac
E Bala <balasubramanyam.evani@gmail.com>
2024-11-10 00:53:12 -0500

    Time: 66 ms (18.16%), Space: 36.5 MB (7.20%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,6 +1,8 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
         dp = []
+        # modification of LIS, O(Nlog3) ~ O(N)
+        # no extra space used
         for num in nums:
             index = bisect_left(dp, num)
             if index == len(dp):
