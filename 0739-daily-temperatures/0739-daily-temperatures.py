d222903a23daefe8aad210b70ed0002e061b7f10
E Bala <balasubramanyam.evani@gmail.com>
2024-11-15 09:29:50 -0500

    Time: 71 ms (99.25%), Space: 25.4 MB (98.94%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,12 +1,25 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack = deque()
+        # stack = deque()
+        # N = len(temperatures)
+        # ret = [0] * N
+        # for i in range(N):
+        #     curr = temperatures[i]
+        #     while stack and stack[-1][1] < curr:
+        #         j, _ = stack.pop()
+        #         ret[j] = i - j
+        #     stack.append((i, curr))
+        # return ret
         N = len(temperatures)
+        currmax = 0
         ret = [0] * N
-        for i in range(N):
+        for i in range(N - 1, -1, -1):
             curr = temperatures[i]
-            while stack and stack[-1][1] < curr:
-                j, _ = stack.pop()
-                ret[j] = i - j
-            stack.append((i, curr))
+            if curr >= currmax:
+                currmax = curr
+            else:
+                days = 1
+                while temperatures[i + days] <= curr:
+                    days += ret[i + days]
+                ret[i] = days
         return ret
