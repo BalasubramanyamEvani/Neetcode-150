7eaa046fe744e0645045643e38a0c528f12f0fc3
E Bala <balasubramanyam.evani@gmail.com>
2024-11-17 02:49:56 -0500

    Time: 49 ms (59.12%), Space: 20.1 MB (24.18%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -12,10 +12,13 @@ class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
         trace = set()
         node = p
+        # store trace from node p to root
         while node:
             trace.add(node)
             node = node.parent
         node = q
+        # then trace node q to root
+        # and get the LCA
         while node:
             if node in trace:
                 return node
