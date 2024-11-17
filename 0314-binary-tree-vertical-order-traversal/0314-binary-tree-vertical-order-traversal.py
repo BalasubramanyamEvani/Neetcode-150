582a06eec550f7b5b2afa7a9cd9513f1ad002fef
E Bala <balasubramanyam.evani@gmail.com>
2024-11-17 02:41:09 -0500

    Time: 0 ms (100.00%), Space: 16.6 MB (44.06%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
@@ -6,10 +6,20 @@
 #         self.right = right
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        # if no root return empty array
         if not root:
             return []
+        # while doing Bfs I will fill up a 2d matrix
+        # for the final traversal
+        
+        # nr will store the number of rows in the matrix
+        # minc, maxc will determine the number of columns required
         nr, minc, maxc = 0, 0, 0
+        # normal BFS stuff
         q = deque()
+        # each item while traversing will store the 
+        # row and column information, root will start at 0, 0
+        # first number is row, second is column
         q.append((0, 0, root))
         bfs = []
         while q:
@@ -19,20 +29,26 @@ class Solution:
             minc = min(minc, c)
             maxc = max(maxc, c)
             if node.left:
+                # when we go left decrease column
                 q.append((r + 1, c - 1, node.left))
             if node.right:
+                # when we go right increas column
                 q.append((r + 1, c + 1, node.right))
-        
+        # now calculate the range to get the final number of
+        # columns
         nc = maxc - minc + 1
+        # number of rows will be + 1
         nr = nr + 1
+        # offset to negative column [-4, 4] -> [0, 8]
         col_offset = -minc 
+        # build the matrix
         ret = [[None] * nc for _ in range(nr)]
         for item in bfs:
             r, c, val = item
             if not ret[r][c + col_offset]:
                 ret[r][c + col_offset] = []
             ret[r][c + col_offset].append(val)
-
+        # traverse the matrix column wise
         res = []
         for i in range(nc):
             tmp = []
@@ -40,6 +56,5 @@ class Solution:
                 if ret[j][i]:
                     tmp.extend(ret[j][i])
             res.append(tmp)
+        # return the result
         return res
-
-            
