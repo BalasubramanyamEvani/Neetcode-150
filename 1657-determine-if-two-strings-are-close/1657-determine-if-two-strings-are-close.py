7e84967527953199c7df236d9ccb5588bf863e48
E Bala <balasubramanyam.evani@gmail.com>
2024-11-18 02:02:06 -0500

    Time: 86 ms (68.24%), Space: 17.3 MB (59.55%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -1,12 +1,15 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
         n1, n2 = len(word1), len(word2)
+        # If the length of the strings are different
         if n1 != n2:
             return False
+        # Check if both contains the same characters
         c1 = Counter(word1)
         c2 = Counter(word2)
         k1, k2 = c1.keys(), c2.keys()
         if k1 != k2:
             return False
+        # Check if we have the same frequency counts
         v1, v2 = Counter(c1.values()), Counter(c2.values())
         return v1 == v2
