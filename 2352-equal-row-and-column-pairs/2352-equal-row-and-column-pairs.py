551fb19c093d0ce5d1cf3850adf8eb0f39f958fb
E Bala <balasubramanyam.evani@gmail.com>
2024-11-18 02:22:57 -0500

    Time: 18 ms (82.90%), Space: 21.3 MB (77.47%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,28 +1,36 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        # Store row values
-        rows = {i: {} for i in range(n)}
+        rcounts = Counter([tuple(row) for row in grid])
+        ret = 0
         for i in range(n):
-            for j in range(n):
-                rows[i][j] = grid[i][j]
+            col = tuple([grid[j][i] for j in range(n)])
+            ret += rcounts.get(col, 0)
+        return ret
         
-        # Store column values
-        cols = {i: {} for i in range(n)}
-        for i in range(n):
-            for j in range(n):
-                cols[i][j] = grid[j][i]
+        # Brute force works, but let's do better
+#         n = len(grid)
+#         # Store row values
+#         rows = {i: {} for i in range(n)}
+#         for i in range(n):
+#             for j in range(n):
+#                 rows[i][j] = grid[i][j]
         
-        # Compare each row with each column
-        counts = 0
-        for i in range(n):
-            for j in range(n):
-                found = True
-                for k in range(n):
-                    if rows[i][k] != cols[j][k]:
-                        found = False
-                        break
-                if found:
-                    counts += 1
-        return counts
-                
+#         # Store column values
+#         cols = {i: {} for i in range(n)}
+#         for i in range(n):
+#             for j in range(n):
+#                 cols[i][j] = grid[j][i]
+        
+#         # Compare each row with each column
+#         counts = 0
+#         for i in range(n):
+#             for j in range(n):
+#                 found = True
+#                 for k in range(n):
+#                     if rows[i][k] != cols[j][k]:
+#                         found = False
+#                         break
+#                 if found:
+#                     counts += 1
+#         return counts
